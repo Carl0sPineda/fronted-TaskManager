ab88538f048f3db76f09ae7ec737fe1dda0f73ab
Carl0sPineda <carlos7712p@gmail.com>
2024-02-11 15:27:03 -0600

    refactor: Restructure form when adding tasks

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,20 +1,14 @@
-import { FormEvent, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FormData } from "../interfaces/task.interface";
 import { useAddTask } from "../hooks/task.mutations";
+import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 
 const AddTask = () => {
   const addTaskMutation = useAddTask();
   const [showModal, setShowModal] = useState<boolean>(false);
   const modalRef = useRef<HTMLDivElement>(null);
-  const [formData, setFormData] = useState<FormData>({
-    title: "",
-    autor: "",
-    statusTask: "",
-    description: "",
-    startDate: "",
-    endDate: "",
-  });
+  const { register, handleSubmit, reset } = useForm<FormData>();
 
   useEffect(() => {
     const handleClickOutside = (e: MouseEvent) => {
@@ -28,30 +22,10 @@ const AddTask = () => {
     };
   }, []);
 
-  const handleChange = (
-    e: React.ChangeEvent<
-      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
-    >
-  ) => {
-    const { name, value } = e.target;
-    setFormData((prevTask) => ({
-      ...prevTask,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const onSubmit = async (data: FormData) => {
     try {
-      await addTaskMutation.mutateAsync(formData);
-      setFormData({
-        title: "",
-        autor: "",
-        statusTask: "",
-        description: "",
-        startDate: "",
-        endDate: "",
-      });
+      await addTaskMutation.mutateAsync(data);
+      reset();
       setShowModal(false);
       toast.success("Tarea agregada con éxito!");
     } catch (error) {
@@ -85,7 +59,7 @@ const AddTask = () => {
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="w-full max-w-md" ref={modalRef}>
               <form
-                onSubmit={handleSubmit}
+                onSubmit={handleSubmit(onSubmit)}
                 className="bg-white shadow-md rounded-xl px-8 pt-6 pb-8 mb-4"
               >
                 <div className="mb-4">
@@ -96,14 +70,12 @@ const AddTask = () => {
                     Título
                   </label>
                   <input
+                    id="title"
                     required
                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="title"
                     type="text"
-                    name="title"
-                    value={formData.title}
-                    onChange={handleChange}
                     placeholder="Ingresa el título de la tarea"
+                    {...register("title")}
                   />
                 </div>
                 <div className="flex">
@@ -115,12 +87,10 @@ const AddTask = () => {
                       Estado
                     </label>
                     <select
+                      id="statusTask"
                       required
                       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="statusTask"
-                      name="statusTask"
-                      value={formData.statusTask} // Aquí se establece el valor seleccionado
-                      onChange={handleChange}
+                      {...register("statusTask")}
                     >
                       <option value="">Seleccione un estado</option>
                       <option value="completado">Completado</option>
@@ -136,14 +106,12 @@ const AddTask = () => {
                       Autor
                     </label>
                     <input
+                      id="autor"
                       required
                       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="autor"
                       type="text"
-                      name="autor"
-                      value={formData.autor}
-                      onChange={handleChange}
                       placeholder="Carlos"
+                      {...register("autor")}
                     />
                   </div>
                 </div>
@@ -156,13 +124,11 @@ const AddTask = () => {
                       Fecha de inicio
                     </label>
                     <input
+                      id="startDate"
                       required
                       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="startDate"
                       type="date"
-                      name="startDate"
-                      value={formData.startDate}
-                      onChange={handleChange}
+                      {...register("startDate")}
                     />
                   </div>
                   <div className="mb-4 w-1/2 ml-2">
@@ -173,13 +139,11 @@ const AddTask = () => {
                       Fecha de fin
                     </label>
                     <input
+                      id="endDate"
                       required
                       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="endDate"
                       type="date"
-                      name="endDate"
-                      value={formData.endDate}
-                      onChange={handleChange}
+                      {...register("endDate")}
                     />
                   </div>
                 </div>
@@ -191,13 +155,11 @@ const AddTask = () => {
                     Descripción
                   </label>
                   <textarea
+                    id="description"
                     required
                     className="shadow appearance-none border h-[7rem] resize-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                    id="description"
-                    name="description"
-                    value={formData.description}
-                    onChange={handleChange}
                     placeholder="Agrega una descripción para la tarea..."
+                    {...register("description")}
                   />
                 </div>
                 <div className="flex items-center justify-between">
